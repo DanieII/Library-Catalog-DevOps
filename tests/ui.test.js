09e39e6369a51352c1d64ad023aa6536b852714e
DanieII <danielbogdanov1748@gmail.com>
2024-03-26 23:49:47 +0200

    Add login test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -23,20 +23,20 @@ test('Verify "Login" button is visible', async ({ page }) => {
   expect(isLoginButtonVisible).toBe(true);
 });
 
-// test('Verify "All Books" link is visible after user login', async ({
-//   page,
-// }) => {
-//   await page.goto("http://localhost:3000/login");
+test('Verify "All Books" link is visible after user login', async ({
+  page,
+}) => {
+  await page.goto("http://localhost:3000/login");
 
-//   await page.fill('input[name="email"]', "peter@abv.bg");
-//   await page.fill('input[name="password"]', "123456");
-//   await page.click('input[type="submit"]');
+  await page.fill('input[name="email"]', "peter@abv.bg");
+  await page.fill('input[name="password"]', "123456");
+  await page.click('input[type="submit"]');
 
-//   const allBooksLink = await page.$('a[href="/catalog"]');
-//   const isAllBooksLinkVisible = await allBooksLink.isVisible();
+  const allBooksLink = await page.$('a[href="/catalog"]');
+  const isAllBooksLinkVisible = await allBooksLink.isVisible();
 
-//   expect(isAllBooksLinkVisible).toBe(true);
-// });
+  expect(isAllBooksLinkVisible).toBe(true);
+});
 
 // test("Login with valid credentials", async ({ page }) => {
 //   await page.goto("http://localhost:3000/login");
